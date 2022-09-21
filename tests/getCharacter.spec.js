49249948e8a427aa9ccb48078539db2f2c96c0b4
feliperaindo <felipe.raindo@gmail.com>
2022-09-20 21:01:33 -0300

    resolve getCharacter

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -41,15 +41,55 @@ Retorno:
 }
 */
 
+const characters = {
+  arya: {
+    name: 'Arya Stark',
+    class: 'Rogue',
+    phrases: ['Not today', 'A girl has no name.'],
+  },
+  brienne: {
+    name: 'Brienne Tarth',
+    class: 'Knight',
+    phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.'],
+  },
+  melissandre: {
+    name: 'Melissandre',
+    class: 'Necromancer',
+    phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'],
+  },
+};
+
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
-  it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
+
     // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
-    // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
-    // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
-    // Teste se o parâmetro não é Case Sensitive, ou seja, independente de conter letras maiúsculas ou minúsculas retorna o mesmo objeto relativo a ele.
-    // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+
+  // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+  test('Vefify return when function receive no parameter', () => {
+    expect(getCharacter()).toBe(undefined);
   });
+
+  // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
+  test('Verify if return a right object when function receive parameter Arya', () => {
+    expect(getCharacter('Arya')).toEqual(characters.arya);
+  });
+
+  // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
+  test('Verify if return a right object when function receive parameter Brienne', () => {
+    expect(getCharacter('Brienne')).toEqual(characters.brienne);
+  });
+
+  // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
+  test('Verify if return a right object when function receive parameter Melissandre', () => {
+    expect(getCharacter('Melissandre')).toEqual(characters.melissandre);
+  });
+
+  // Teste se o parâmetro não é Case Sensitive, ou seja, independente de conter letras maiúsculas ou minúsculas retorna o mesmo objeto relativo a ele.
+  test('Verify if function is case sensitive', () => {
+    expect(getCharacter('melisSaNdre')).toEqual(characters.melissandre);
+  });
+
+  // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+  test('Varify if returns undefined when funciton receive a name not registered', () => {
+    expect(getCharacter('bruno')).toBeUndefined();
+  })
 });
